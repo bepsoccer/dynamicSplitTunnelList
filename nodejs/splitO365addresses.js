2228ba8e82d7c48c378855652e46c8dd609ed182
bepsoccer <brad@parker1723.com>
2018-04-02 14:44:37 -0500

    working framework
    still need to figure out cidr-clean declarations, but framework is
    there to report errors to the call.

--- nodejs/splitO365addresses.js
+++ nodejs/splitO365addresses.js
@@ -6,16 +6,14 @@ splitO365addresses.prototype.isPublic = true;
 var logger = require('f5-logger').getInstance();
 var Client = require('node-rest-client').Client;
 var includes = require('array-includes');
-var cidrClean = require('cidr-clean');
+//var cidrClean = require('cidr-clean');
 
 /**
 * handle onGet HTTP request
 */
 splitO365addresses.prototype.onGet = function(restOperation) {
   var basicAuthToken = restOperation.getBasicAuthorization();
-    logger.info("gathered basic auth " + basicAuthToken);
-    logger.info("about to set settings");
-    var settings = require('settings.json');
+    var settings = require('./settings.json');
 
     var options = {
       mimetypes: {
@@ -24,7 +22,6 @@ splitO365addresses.prototype.onGet = function(restOperation) {
     };
     var client = new Client(options);
     client.parsers.find("XML").options= {"mergeAttrs": true};
-logger.info(settings);
     client.get(settings.o365addressURL, function (data, response) {
       var newlist = [];
       for(var i = 0; i < data.products.product.length; i++)
@@ -43,7 +40,7 @@ logger.info(settings);
         }
       }
       if (newlist.length) {
-        var newlist = cidrClean(newlist);
+        //var newlist = cidrClean(newlist);
         var addresses = [];
         newlist.forEach(function(value) {
           var tempobj = {'subnet': value};
@@ -67,16 +64,34 @@ logger.info(settings);
           }
         };
         var client = new Client(options_auth);
-        client.patch("https://localhost:8100/mgmt/tm/apm/resource/network-access/${NA}", args, function (data, response) {
-          logger.info(response);
-          restOperation.setBody(response.statusCode + " " + response.statusMessage);
-          this.completeRestOperation(restOperation);
+        var req = client.patch("http://localhost:8100/mgmt/tm/apm/resource/network-access/${NA}", args, function (data, response) {
+          logger.info(data);
+          restOperation.setBody(JSON.stringify(data));
+          //restOperation.setBody(JSON.stringify(response.statusCode + " " + response.statusMessage));
+          splitO365addresses.prototype.completeRestOperation(restOperation);
+        }).on('error', function (err) {
+            logger.info('something went wrong on the request', err.request.options);
+            restOperation.setBody(JSON.stringify(err.request.options));
+            splitO365addresses.prototype.completeRestOperation(restOperation);
+        });
+        req.on('requestTimeout', function (req) {
+            logger.info('request has expired');
+            req.abort();
+        });
+         
+        req.on('responseTimeout', function (res) {
+            logger.info('response has expired');
+            restOperation.setBody(JSON.stringify('response has expired'));
+            splitO365addresses.prototype.completeRestOperation(restOperation);
+        });
+         
+        req.on('error', function (err) {
+            logger.info('request error', err);
+            restOperation.setBody(JSON.stringify(err));
+            splitO365addresses.prototype.completeRestOperation(restOperation);
         });
       }
     });
-  //restOperation.setBody(JSON.stringify(newRestOperation("get", "http://localhost:8100/mgmt/tm/ltm/profile/", basicAuthToken) ));
-  //restOperation.setBody(JSON.stringify(basicAuthToken));
-  //restOperation.setBody(JSON.stringify(newCall()));
   //this.completeRestOperation(restOperation);
 };
 
